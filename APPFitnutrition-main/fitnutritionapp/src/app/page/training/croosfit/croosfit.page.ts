de5198720c9ab47a2025b1cc9629df6c08c66363
tomasolivares2704 <129810321+tomasolivares2704@users.noreply.github.com>
2023-08-28 19:39:34 -0400

    CREACION NUEVA RAMIFICACION

---
+++ APPFitnutrition-main/fitnutritionapp/src/app/page/training/croosfit/croosfit.page.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-croosfit',
+  templateUrl: './croosfit.page.html',
+  styleUrls: ['./croosfit.page.scss'],
+})
+export class CroosfitPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
